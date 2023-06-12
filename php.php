8cd654023826100dde288293622cd4e2877f2315
SRIVARSHANK <117512968+SRIVARSHANK@users.noreply.github.com>
2023-06-12 10:23:51 +0530

    Add files via upload

---
+++ php.php
@@ -0,0 +1,29 @@
+<?php
+// Establish database connection
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Store form data in the database
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Registration successful!";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
